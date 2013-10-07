2148bfc5a7b93c212139869ea4d47e54de70f927
Olle Werme <olle.werme@gmail.com>
2013-10-07 13:55:29 +0200

    Add config for autojump

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -46,3 +46,5 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
