512ba0acadda9fba3613e9a5c99458cd99aacb30
werme <olle.werme@gmail.com>
2013-10-07 11:30:37 +0200

    Add alias for cd:ing into dotfiles directory

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -28,3 +28,5 @@ alias localip="ifconfig -a | grep-o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9
 # Hide/show all desktop icons (useful when presenting)
 alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
 alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+alias dotfiles="cd ~/.dotfiles"
